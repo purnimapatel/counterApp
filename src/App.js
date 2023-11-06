8486bb971e8eaf355a009808c2f8f4003b16cb66
= <=>
2023-11-06 14:52:35 +0530

    counter App Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter APPLICATION</h1>
+      {counter}
+      <br></br>
+      <button className='btn2' onClick={()=>setCounter(counter-1)}>Decrease</button>
+      <br></br>
+      <button className='btn1' onClick={()=>setCounter(counter+1)}>Increase</button>
+      
+      
     </div>
   );
 }
